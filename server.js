25fef811516061f4567b36b836a990180665ceef
Shusandesh GC <gcshusandesh@gmail.com>
2025-04-20 15:27:52 +0545

    v2.0.0

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from Docker container!");
+
+  fs.readFile("/app/data/test.txt", "utf8", (err, data) => {
+    if (err) {
+      res.end("Error reading file or no data available.");
+    } else {
+      res.end("Data from file: " + data);
+    }
+  });
 });
 
 server.listen(3000, () => {
-  console.log("Server running on port 3000");
+  console.log("Server running on port 3000 - Version 2.0.0");
 });
