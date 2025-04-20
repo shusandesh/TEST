928262d919868d772599e39f8cb537a3e8a2d632
Shusandesh GC <gcshusandesh@gmail.com>
2025-04-20 15:39:41 +0545

    refinement

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const http = require("http");
 const fs = require("fs");
+const path = "/app/data/test.txt";
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.writeHead(200, { "Content-Type": "text/html" });
 
-  fs.readFile("/app/data/test.txt", "utf8", (err, data) => {
+  fs.readFile(path, "utf8", (err, data) => {
     if (err) {
-      res.end("Error reading file or no data available.");
+      res.end(`
+        <h1>🚫 No data found</h1>
+        <p>Error: ${err.message}</p>
+        <p>Expected data file: <code>${path}</code></p>
+      `);
     } else {
-      res.end("Data from file: " + data);
+      res.end(`
+        <h1>🚀 Docker App Version 2.0.0</h1>
+        <p><strong>Persisted Data:</strong></p>
+        <pre>${data}</pre>
+      `);
     }
   });
 });
